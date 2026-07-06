2943765c3c33f9a145be3c7165b56a760019d230
lhamm470 <hammerluke01@gmail.com>
2026-07-06 22:03:00 +1000

    Can now pick up and drop coins
    Coins have rigidbodys now and you drop them in front of you. Don't know what happens if there is already an object where the coin gets dropped (probably bug)

--- Assets/Scripts/Interactables/Coin.cs
+++ Assets/Scripts/Interactables/Coin.cs
@@ -5,19 +5,50 @@ using UnityEngine;
 
 public class Coin : NetworkBehaviour, IInteractable
 {
-    public string GetInteractionPromptText() {
-        return "Press E to pick up coin";
-    }
-
-    public void Interact() {
-        Player.localPlayer.score += ObjectiveScores.deliveryScore;
-        Player.localPlayer.scoreText.text = $"Score: {Player.localPlayer.score}";
-        DespawnServerRpc();
-    }
-
-    public void Update() {
+    bool isBeingCarried = false;
+    public void Update()
+    {
         ((IInteractable)this).TryInteract();
+
+        // If coin is being carried:
+        if (Player.localPlayer && Player.localPlayer.isCarryingCoin && isBeingCarried)
+        {
+            // Lock position to rats spine
+            Transform spine = Player.localPlayer.transform.Find("Armature/Hip/Spine");
+            transform.position = spine.TransformPoint(new Vector3(0.005f, 0, 0));
+            transform.rotation = spine.rotation * Quaternion.Euler(0, 0, 90);
+
+            // Drop coin
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                isBeingCarried = false;
+                Player.localPlayer.isCarryingCoin = false;
+                transform.position = Player.localPlayer.transform.TransformPoint(new Vector3(0, 2f, 3f));
+                transform.SetParent(GameObject.Find("Coin Container").transform);
+                this.GetComponent<BoxCollider>().enabled = true;
+                this.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
     }
+    public string GetInteractionPromptText()
+    {
+        return Player.localPlayer.isCarryingCoin ? "Already carrying coin" : "Press E to pick up coin";
+    }
+
+    public void Interact()
+    {
+        if (!Player.localPlayer.isCarryingCoin)
+        {
+            Player.localPlayer.score += ObjectiveScores.deliveryScore;
+            Player.localPlayer.scoreText.text = $"Score: {Player.localPlayer.score}";
+            transform.SetParent(Player.localPlayer.transform);
+            Player.localPlayer.isCarryingCoin = true;
+            this.GetComponent<BoxCollider>().enabled = false;
+            this.GetComponent<Rigidbody>().useGravity = false;
+            isBeingCarried = true;
+        }
+    }
+
 
     [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
     private void DespawnServerRpc()
