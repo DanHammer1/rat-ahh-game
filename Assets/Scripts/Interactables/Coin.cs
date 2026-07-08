51396c793ff434f46e8841e0e8122fa5b54a8169
lhamm470 <hammerluke01@gmail.com>
2026-07-08 21:48:55 +1000

    Coin syncing on drop improved
    better but still buggy. Coin on ground can also be pushed, and doesn't sync when pushed manually.

--- Assets/Scripts/Interactables/Coin.cs
+++ Assets/Scripts/Interactables/Coin.cs
@@ -36,15 +36,19 @@ public class Coin : NetworkBehaviour, IInteractable
     [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
     public void DropCoinRpc()
     {
-        ToggleIsBeingCarriedRpc();
-        Player.localPlayer.ToggleIsCarryingCoinRpc();
-        Player.localPlayer.ToggleIsCarryingCoinClientRpc();
-        transform.position = Player.localPlayer.transform.TransformPoint(new Vector3(0, 2f, 3f));
-        SetCoinParentRpc(GameObject.Find("Coin Container").GetComponent<NetworkObject>());
-        this.GetComponent<BoxCollider>().enabled = true;
-        this.GetComponent<Rigidbody>().useGravity = true;
-        Player.localPlayer.transform.GetComponent<Movement>().MultiplyMoveSpeedRpc(1 / Constants.carryingCoinMoveSpeedMultiplier);
+        NetworkObject player;
+        playerCarryingCoin.Value.TryGet(out player);
 
+        ToggleIsBeingCarriedRpc();
+        player.transform.GetComponent<Player>().ToggleIsCarryingCoinRpc();
+        // player.transform.GetComponent<Player>().ToggleIsCarryingCoinClientRpc();
+        transform.position = player.transform.TransformPoint(new Vector3(0, 2f, 3f));
+        SetCoinParentRpc(GameObject.Find("Coin Container").GetComponent<NetworkObject>());
+        ToggleBoxColliderRpc();
+        ToggleRigidbodyGravityRpc();
+        player.transform.GetComponent<Movement>().MultiplyMoveSpeedRpc(1 / Constants.carryingCoinMoveSpeedMultiplier);
+
+        Debug.Log(Player.localPlayer.isCarryingCoin.Value);
         pickUpProgress = 0;
     }
 
@@ -72,7 +76,7 @@ public class Coin : NetworkBehaviour, IInteractable
         transform.position = spine.TransformPoint(new Vector3(0.005f, 0, 0));
         transform.rotation = spine.rotation * Quaternion.Euler(0, 0, 90);
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Player.localPlayer.NetworkObject == player && Input.GetKeyDown(KeyCode.Q))
         {
             DropCoinRpc();
         }
@@ -90,14 +94,27 @@ public class Coin : NetworkBehaviour, IInteractable
         {
             SetCoinParentRpc(Player.localPlayer.GetComponent<NetworkObject>());
             Player.localPlayer.ToggleIsCarryingCoinRpc();
-            Player.localPlayer.ToggleIsCarryingCoinClientRpc();
-            this.GetComponent<BoxCollider>().enabled = false;
-            this.GetComponent<Rigidbody>().useGravity = false;
+            // Player.localPlayer.ToggleIsCarryingCoinClientRpc();
+            ToggleBoxColliderRpc();
+            ToggleRigidbodyGravityRpc();
             ToggleIsBeingCarriedRpc();
             SetPlayerCarryingCoinRpc(Player.localPlayer.gameObject);
             Player.localPlayer.transform.GetComponent<Movement>().MultiplyMoveSpeedRpc(Constants.carryingCoinMoveSpeedMultiplier);
+            Debug.Log(Player.localPlayer.isCarryingCoin.Value);
         }
     }
+
+    [Rpc(SendTo.Everyone)]
+    private void ToggleRigidbodyGravityRpc()
+    {
+        this.GetComponent<Rigidbody>().useGravity = !this.GetComponent<Rigidbody>().useGravity;
+    }
+    [Rpc(SendTo.Everyone)]
+    private void ToggleBoxColliderRpc()
+    {
+        this.GetComponent<BoxCollider>().enabled = !this.GetComponent<BoxCollider>().enabled;
+    }
+
 
     public void UpdateProgress()
     {
