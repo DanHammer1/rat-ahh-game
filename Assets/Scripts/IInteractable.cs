abd407085716af40ba75f131f762fb85d2e6f879
lhamm470 <hammerluke01@gmail.com>
2026-07-12 23:56:19 +1000

    Made interactable raycase Spherecast
    Changed to spherecast to make it easier to interact with interactables. Contains annoying bug where is spherecast starts inside interactable it returns false. Temp fix keep original Raycast as secondary check, but there is still a zone very close to interactable where you have to be looking exactly at it to interact

--- Assets/Scripts/IInteractable.cs
+++ Assets/Scripts/IInteractable.cs
@@ -12,93 +12,147 @@ public interface IInteractable
 
     public void UpdateProgress();
 
-    public bool CheckExtraInteractionConditions() {
+    public bool CheckExtraInteractionConditions()
+    {
         return true;
     }
 
-    public void TryInteract() {
+    public void TryInteract()
+    {
         if (CheckInteractionShouldTrigger()) Interact();
         TryUpdateProgress();
     }
 
-    public static bool CheckPlayerFacingInteractableObject() {
+    public static bool CheckPlayerFacingInteractableObject()
+    {
         if (Player.localPlayer == null) return false;
 
         RaycastHit hit;
-        if (Physics.Raycast(
+        if (Physics.SphereCast(
             Player.localPlayer.viewPosition.transform.position,
+            0.1f,
             PlayerCamera.mainCamera.transform.forward,
             out hit,
-            1f, LayerMask.GetMask("InteractableObject"))) {
+            1f, LayerMask.GetMask("InteractableObject")))
+        {
 
             GameObject interactPrompt = GameObject.FindWithTag("InteractionPrompt");
             IInteractable implementationScript = hit.collider.gameObject.GetComponent<IInteractable>();
             string newInteractText = implementationScript.GetInteractionPromptText();
             interactPrompt.GetComponent<TextMeshProUGUI>().text = newInteractText;
             implementationScript.UpdateProgressBar(implementationScript.GetProgress());
-            
+
             return (!Player.localPlayer.dead && implementationScript.CheckExtraInteractionConditions());
-            }
-        
+        }
+
+        // Second check incase spherecast spawns on interactable and returns false
+        RaycastHit secondHit;
+        if (Physics.Raycast(
+            Player.localPlayer.viewPosition.transform.position,
+            PlayerCamera.mainCamera.transform.forward,
+            out secondHit,
+            1f, LayerMask.GetMask("InteractableObject")))
+        {
+
+            GameObject interactPrompt = GameObject.FindWithTag("InteractionPrompt");
+            IInteractable implementationScript = secondHit.collider.gameObject.GetComponent<IInteractable>();
+            string newInteractText = implementationScript.GetInteractionPromptText();
+            interactPrompt.GetComponent<TextMeshProUGUI>().text = newInteractText;
+            implementationScript.UpdateProgressBar(implementationScript.GetProgress());
+
+            return (!Player.localPlayer.dead && implementationScript.CheckExtraInteractionConditions());
+        }
+
         return false;
     }
 
-    public bool CheckPlayerInRange() {
+    public bool CheckPlayerInRange()
+    {
         if (Player.localPlayer == null) return false;
 
         RaycastHit hit;
-        if (Player.localPlayer.viewPosition == null) {
+        if (Player.localPlayer.viewPosition == null)
+        {
             Debug.LogError("viewPosition is gone.");
             return false;
         }
-        else if (PlayerCamera.mainCamera == null) {
+        else if (PlayerCamera.mainCamera == null)
+        {
             Debug.LogError("mainCamera is gone.");
         }
+        if (Physics.SphereCast(
+            Player.localPlayer.viewPosition.transform.position,
+            0.1f,
+            PlayerCamera.mainCamera.transform.forward,
+            out hit,
+            1f, LayerMask.GetMask("InteractableObject")))
+        {
+            if (hit.collider.gameObject == null) return false;
+
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject.GetComponent<IInteractable>() == this)
+            {
+                return true;
+            }
+        }
+
+        // Second check incase spherecast spawns on interactable and returns false
         if (Physics.Raycast(
             Player.localPlayer.viewPosition.transform.position,
             PlayerCamera.mainCamera.transform.forward,
             out hit,
-            1f, LayerMask.GetMask("InteractableObject"))) {
-                if (hit.collider.gameObject == null) return false;
+            1f, LayerMask.GetMask("InteractableObject")))
+        {
+            if (hit.collider.gameObject == null) return false;
 
-                GameObject hitObject = hit.collider.gameObject;
-                if (hitObject.GetComponent<IInteractable>() == this) {
-                    return true;
-                }
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject.GetComponent<IInteractable>() == this)
+            {
+                return true;
             }
-        
+        }
+
         return false;
     }
 
-    public bool CheckInteractionShouldTrigger() {
+    public bool CheckInteractionShouldTrigger()
+    {
         return (CheckPlayerInRange() && Input.GetKey(KeyCode.E) && GetProgress() >= 1);
     }
 
-    public void TryUpdateProgress() {
-        if (CheckPlayerInRange() && Input.GetKey(KeyCode.E)) {
+    public void TryUpdateProgress()
+    {
+        if (CheckPlayerInRange() && Input.GetKey(KeyCode.E))
+        {
             UpdateProgress();
-        } else {
+        }
+        else
+        {
             OnInteractingExit();
         }
     }
 
-    public void OnInteractingExit() {}
+    public void OnInteractingExit() { }
 
-    public void UpdateProgressBar(float progress) {
+    public void UpdateProgressBar(float progress)
+    {
         GameObject.FindWithTag("ProgressFillBar").GetComponent<Image>().fillAmount = progress;
     }
 
-    public static void TryDisplayInteractionText() {
+    public static void TryDisplayInteractionText()
+    {
         GameObject interactPrompt = GameObject.FindWithTag("InteractionPrompt");
         GameObject interactBackground = GameObject.FindWithTag("InteractionPromptBackground");
         GameObject interactFillBar = GameObject.FindWithTag("ProgressFillBar");
 
-        if (interactPrompt == null) {
+        if (interactPrompt == null)
+        {
             Debug.LogError("No prompt found.");
             return;
         }
 
-        if (!CheckPlayerFacingInteractableObject()) {
+        if (!CheckPlayerFacingInteractableObject())
+        {
             interactPrompt.GetComponent<TextMeshProUGUI>().enabled = false;
             interactBackground.GetComponent<Image>().enabled = false;
             interactFillBar.GetComponent<Image>().enabled = false;
