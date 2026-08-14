2981c71f943fef3de9458387420069989a0c287e
lhamm470 <hammerluke01@gmail.com>
2026-08-14 17:43:06 +1000

    switching roles half works
    works but gives erros

--- Assets/Scripts/Lobby/SwitchRole.cs
+++ Assets/Scripts/Lobby/SwitchRole.cs
@@ -18,20 +18,28 @@ public class SwitchRole : NetworkBehaviour {
     }
 
     void SwitchRoles() {
-        if (!IsServer) return;
+        // if (!IsServer) return;
 
         GameManager.PlayerRole playerRole = GameManager.GetLocalRole();
         if (playerRole == role) {
             return;
         }
         ulong clientId = GameManager.GetLocalId();
+        EditClientRoleServerRpc(clientId, role);
 
         NetworkClient client = NetworkManager.Singleton.ConnectedClients[clientId];
         client.PlayerObject.Despawn(true);
         GameManager.Instance.SpawnPlayer(role, clientId);
     }
 
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    void EditClientRoleServerRpc(ulong clientId, GameManager.PlayerRole preference) {
+        GameManager.Instance.clientRoles[GameManager.Instance.clientIds.IndexOf(clientId)]
+                = (int)(preference);
+    }
+
     void OnTriggerEnter() {
+        Debug.Log("switched");
         SwitchRoles();
     }
 }
