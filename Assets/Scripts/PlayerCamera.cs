a181c47bffa5854f82513bf7efce27a132202b0f
lhamm470 <hammerluke01@gmail.com>
2026-09-19 19:44:02 +1000

    updated swing animation

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -163,14 +163,14 @@ public class PlayerCamera : MonoBehaviour {
             cinemachinePositionComposer.Damping = new Vector3(0.4f, 0.4f, 0.4f);
         }
 
-        /*if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha5))
             Time.timeScale = 0.05f;
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha6))
             Time.timeScale = 0.2f;
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            Time.timeScale = 1f;*/
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            Time.timeScale = 1f;
 
         cinemachineInputAxisController.Controllers[0].Input.Gain = mouseSensitivity * movement.movementRecoveryMultiplier;
         cinemachineInputAxisController.Controllers[1].Input.Gain = -mouseSensitivity * movement.movementRecoveryMultiplier;
