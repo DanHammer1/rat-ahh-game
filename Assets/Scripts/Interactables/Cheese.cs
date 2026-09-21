96baaf846d322a01dba6b27733fc7310bac6eab4
lhamm470 <hammerluke01@gmail.com>
2026-09-21 15:08:59 +1000

    decrease cheese interaction time 6 -> 4

--- Assets/Scripts/Interactables/Cheese.cs
+++ Assets/Scripts/Interactables/Cheese.cs
@@ -16,7 +16,7 @@ public class Cheese : NetworkBehaviour, IInteractable {
     public Action onPlayerSeesObject;
 
     private float eatProgress = 0;
-    private float totalInteractionTime = 6f;
+    private float totalInteractionTime = 4f;
 
     public override void OnNetworkSpawn() {
         onPlayerSeesObject += () => ObjectManager.TakeAwaySpectral(transform.Find("Renderer").gameObject);
