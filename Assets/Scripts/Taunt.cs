8d91f8c88c9eee603aa23fafb1dc78c071bccba5
DanHammer1 <86539702+DanHammer1@users.noreply.github.com>
2026-09-26 13:12:25 +1000

    bugfix

--- Assets/Scripts/Taunt.cs
+++ Assets/Scripts/Taunt.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using Unity.Netcode;
 using System.Collections;
-public class Taunt : NetworkBehaviour {
+public class Taunt : NetworkBehaviour
+{
     public KeyCode hotkey;
     public float cooldown;
     private bool playable = true;
-    public enum TauntType {
+    public enum TauntType
+    {
         Soft,
         Medium,
         Loud
@@ -13,35 +15,42 @@ public class Taunt : NetworkBehaviour {
 
     public TauntType tauntType;
 
-    void Update() {
+    void Update()
+    {
         if (!IsOwner) return;
 
         Player player = GetComponent<Player>();
         TryGetComponent<RatPlayer>(out RatPlayer ratPlayer);
-        if (Input.GetKeyDown(hotkey) && playable && !player.isInUIMenu && (!ratPlayer?.dead.Value ?? true) && (!ratPlayer?.isGhost ?? true)) {
+        if (Input.GetKeyDown(hotkey) && playable && !player.isInUIMenu && (!ratPlayer?.dead.Value ?? true) && (!ratPlayer?.isGhost ?? true))
+        {
             playable = false;
             StartCoroutine(ReactivateTaunt());
             GameManager.PlayGlobalSoundEffectInWorld(GetSfxFromTaunt(tauntType), transform.position);
 
-            if (tauntType == TauntType.Loud) {
+            if (tauntType == TauntType.Loud)
+            {
                 ObjectManager.MakeObjectSpectralForEveryone(Player.localPlayer.gameObject);
                 StartCoroutine(DeactivateGlow());
             }
         }
     }
 
-    private IEnumerator ReactivateTaunt() {
+    private IEnumerator ReactivateTaunt()
+    {
         yield return new WaitForSeconds(cooldown);
         playable = true;
     }
 
-    private IEnumerator DeactivateGlow() {
+    private IEnumerator DeactivateGlow()
+    {
         yield return new WaitForSeconds(5);
-        ObjectManager.TakeObjectSpectralForEveryone(Player.localPlayer.gameObject);
+        ObjectManager.TakeObjectSpectralForEveryone(Player.localPlayer.gameObject, "OutlinedObjects");
     }
 
-    public Assets.SfxType GetSfxFromTaunt(TauntType taunt) {
-        Assets.SfxType sfx = taunt switch {
+    public Assets.SfxType GetSfxFromTaunt(TauntType taunt)
+    {
+        Assets.SfxType sfx = taunt switch
+        {
             TauntType.Soft => Assets.SfxType.ratTauntSoft,
             TauntType.Medium => Assets.SfxType.ratTauntMedium,
             TauntType.Loud => Assets.SfxType.ratTauntLoud,
